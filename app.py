441e402971b7a005c4d350bca98a84541e0f9aae
cynnancy <91360712+cynnancy@users.noreply.github.com>
2021-12-25 14:28:32 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -41,12 +41,10 @@ def callback():
 from linebot.models import FlexSendMessage
 from linebot.models import TemplateSendMessage
 
-
-
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text = event.message.text
+    fruit = {"aple": '20', 'orange': "30"}
     #推播前五大當沖股
     top_five = "apple"
     if re.match('前五大', message):
@@ -56,6 +54,9 @@ def handle_message(event):
         error_message = '不好意思'
         line_bot_api.reply_message(event.reply_token, TextSendMessage(error_message))
     
+    if message in fruit:
+        rep = 'ko'
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(rep))
     #推播新聞
     #if re.match('新聞', message):
 
